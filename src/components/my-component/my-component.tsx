fae7b2c58b47ec94ee953ba6547320ec2834899e
Rob Ruiz <Rob.Ruiz@clsinvest.local>
2019-01-02 16:23:31 -0600

    first commit

---
+++ src/components/my-component/my-component.tsx
@@ -0,0 +1,32 @@
+import { Component, Prop } from '@stencil/core';
+import { format } from '../../utils/utils';
+
+@Component({
+  tag: 'ws-audio-player',
+  styleUrl: 'ws-audio-player.css',
+  shadow: true
+})
+export class MyComponent {
+  /**
+   * The first name
+   */
+  @Prop() first: string;
+
+  /**
+   * The middle name
+   */
+  @Prop() middle: string;
+
+  /**
+   * The last name
+   */
+  @Prop() last: string;
+
+  private getText(): string {
+    return format(this.first, this.middle, this.last);
+  }
+
+  render() {
+    return <div>Hello, World! I'm {this.getText()}</div>;
+  }
+}
