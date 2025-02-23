8172839d4617c4ddb26db353019a5caea262d97a
robruiz <rob.c.ruiz@gmail.com>
2025-02-22 19:24:09 -0600

    cleaning up and adding documentation

--- src/components.d.ts
+++ src/components.d.ts
@@ -6,6 +6,43 @@
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
+    /**
+     * This class represents a custom audio player component built using WaveSurfer.js, offering
+     * functionalities such as play/pause, looping, and region-based audio playback control.
+     * Properties managed within the component allow customization of audio appearance,
+     * behavior, and additional event handling for a seamless user experience.
+     * The component relies on the WaveSurfer.js library to render audio waveforms and manage audio playback.
+     * A variety of features including looping regions, event dispatching, and keyboard interactions are supported.
+     * Props:
+     * - `audio`: URL string of the audio file to be played.
+     * - `color`: String for the waveform color.
+     * - `progress`: String for the progress bar color (default: "#666666").
+     * - `theme`: String indicating the UI theme of the audio player (default: "basic").
+     * - `audioTitle`: Title of the audio track.
+     * - `duration`: Duration of the audio track (mutable).
+     * - `height`: String specifying the height of the waveform.
+     * - `resolution`: Resolution value for waveform rendering (default: 100).
+     * States:
+     * - `isPlaying`: Boolean indicating the current playback state of the audio.
+     * - `curTime`: Current playback time of the audio in string format.
+     * - `isLooping`: Boolean indicating whether loop mode is active.
+     * - `audioRegions`: Array representing the defined audio regions for looping or selection.
+     * Methods:
+     * - `playpause()`: Toggles between play and pause states for the current audio.
+     * - `toggleLoop()`: Enables or disables a full-track looping feature with visual regions.
+     * - `enableFullTrackLoop()`: Configures and starts a full-track loop sequence using WaveSurfer.js regions.
+     * - `setLoop(enable)`: Globally enables or disables the loop functionality for the audio player.
+     * - `create()`: Initializes the WaveSurfer player instance with essential configurations and event listeners.
+     * Event Emission:
+     * - Emits the `wsAudioPlaying` event when playback starts, sharing audio source details and title.
+     * Keyboard Control:
+     * - Handles the spacebar keydown event for toggling play/pause, ensuring the key press is valid only within the context of the component.
+     * WaveSurfer Instance:
+     * - The `wsPlayer` instance provides direct access to WaveSurfer.js methods, such as playback control and region management.
+     * - Regions are handled using the `RegionsPlugin` for highlighting and looping selected portions of audio.
+     * Shadow DOM:
+     * - The component uses Shadow DOM to encapsulate styles and provide a scoped structure for rendering.
+     */
     interface WsAudioPlayer {
         "audio": string;
         "audioTitle": string;
@@ -14,19 +51,77 @@ export namespace Components {
          */
         "audio-title"?: string;
         "color": string;
+        /**
+          * Creates a WaveSurfer player instance and initializes it with the specified options. Sets up event listeners for playback, audio processing, and global playback control. Initializes the audio waveform, duration, and playback state.
+          * @return A promise that resolves when the WaveSurfer player is ready.
+         */
         "create": () => Promise<void>;
         "duration": string;
+        /**
+          * Enables a looping feature for the entire audio track using a region. The looping is manually managed through time updates. This method ensures a region is created to represent the loop and handles playback accordingly.
+          * @return A Promise that resolves when the loop has been successfully set up, or logs errors for invalid states.
+         */
         "enableFullTrackLoop": () => Promise<void>;
         "height": string;
+        /**
+          * Toggles the playback state of the media player. If playback is currently ongoing, it will pause; otherwise, it will start/resume.
+          * @return A promise that resolves when the playback state toggling, state updates, and event emission are complete.
+         */
         "playpause": () => Promise<void>;
         "progress": string;
         "resolution": number;
+        /**
+          * Enables or disables the loop feature for the media player. When enabled, the media will replay automatically after it finishes.
+          * @param enable - A boolean flag to enable (true) or disable (false) the loop functionality.
+          * @return A promise that resolves when the loop functionality is successfully updated.
+         */
         "setLoop": (enable: boolean) => Promise<void>;
         "theme": string;
+        /**
+          * Toggles the loop feature of the player. Enables or disables looping for the full track. When enabled, the loop creates a UI region and subscribes to the timeupdate event for continuous looping within the set region. When disabled, it clears the region and unsubscribes from the related event listeners.
+          * @return A promise that resolves when the looping state has been successfully toggled.
+         */
         "toggleLoop": () => Promise<void>;
     }
 }
 declare global {
+    /**
+     * This class represents a custom audio player component built using WaveSurfer.js, offering
+     * functionalities such as play/pause, looping, and region-based audio playback control.
+     * Properties managed within the component allow customization of audio appearance,
+     * behavior, and additional event handling for a seamless user experience.
+     * The component relies on the WaveSurfer.js library to render audio waveforms and manage audio playback.
+     * A variety of features including looping regions, event dispatching, and keyboard interactions are supported.
+     * Props:
+     * - `audio`: URL string of the audio file to be played.
+     * - `color`: String for the waveform color.
+     * - `progress`: String for the progress bar color (default: "#666666").
+     * - `theme`: String indicating the UI theme of the audio player (default: "basic").
+     * - `audioTitle`: Title of the audio track.
+     * - `duration`: Duration of the audio track (mutable).
+     * - `height`: String specifying the height of the waveform.
+     * - `resolution`: Resolution value for waveform rendering (default: 100).
+     * States:
+     * - `isPlaying`: Boolean indicating the current playback state of the audio.
+     * - `curTime`: Current playback time of the audio in string format.
+     * - `isLooping`: Boolean indicating whether loop mode is active.
+     * - `audioRegions`: Array representing the defined audio regions for looping or selection.
+     * Methods:
+     * - `playpause()`: Toggles between play and pause states for the current audio.
+     * - `toggleLoop()`: Enables or disables a full-track looping feature with visual regions.
+     * - `enableFullTrackLoop()`: Configures and starts a full-track loop sequence using WaveSurfer.js regions.
+     * - `setLoop(enable)`: Globally enables or disables the loop functionality for the audio player.
+     * - `create()`: Initializes the WaveSurfer player instance with essential configurations and event listeners.
+     * Event Emission:
+     * - Emits the `wsAudioPlaying` event when playback starts, sharing audio source details and title.
+     * Keyboard Control:
+     * - Handles the spacebar keydown event for toggling play/pause, ensuring the key press is valid only within the context of the component.
+     * WaveSurfer Instance:
+     * - The `wsPlayer` instance provides direct access to WaveSurfer.js methods, such as playback control and region management.
+     * - Regions are handled using the `RegionsPlugin` for highlighting and looping selected portions of audio.
+     * Shadow DOM:
+     * - The component uses Shadow DOM to encapsulate styles and provide a scoped structure for rendering.
+     */
     interface HTMLWsAudioPlayerElement extends Components.WsAudioPlayer, HTMLStencilElement {
     }
     var HTMLWsAudioPlayerElement: {
@@ -38,6 +133,43 @@ declare global {
     }
 }
 declare namespace LocalJSX {
+    /**
+     * This class represents a custom audio player component built using WaveSurfer.js, offering
+     * functionalities such as play/pause, looping, and region-based audio playback control.
+     * Properties managed within the component allow customization of audio appearance,
+     * behavior, and additional event handling for a seamless user experience.
+     * The component relies on the WaveSurfer.js library to render audio waveforms and manage audio playback.
+     * A variety of features including looping regions, event dispatching, and keyboard interactions are supported.
+     * Props:
+     * - `audio`: URL string of the audio file to be played.
+     * - `color`: String for the waveform color.
+     * - `progress`: String for the progress bar color (default: "#666666").
+     * - `theme`: String indicating the UI theme of the audio player (default: "basic").
+     * - `audioTitle`: Title of the audio track.
+     * - `duration`: Duration of the audio track (mutable).
+     * - `height`: String specifying the height of the waveform.
+     * - `resolution`: Resolution value for waveform rendering (default: 100).
+     * States:
+     * - `isPlaying`: Boolean indicating the current playback state of the audio.
+     * - `curTime`: Current playback time of the audio in string format.
+     * - `isLooping`: Boolean indicating whether loop mode is active.
+     * - `audioRegions`: Array representing the defined audio regions for looping or selection.
+     * Methods:
+     * - `playpause()`: Toggles between play and pause states for the current audio.
+     * - `toggleLoop()`: Enables or disables a full-track looping feature with visual regions.
+     * - `enableFullTrackLoop()`: Configures and starts a full-track loop sequence using WaveSurfer.js regions.
+     * - `setLoop(enable)`: Globally enables or disables the loop functionality for the audio player.
+     * - `create()`: Initializes the WaveSurfer player instance with essential configurations and event listeners.
+     * Event Emission:
+     * - Emits the `wsAudioPlaying` event when playback starts, sharing audio source details and title.
+     * Keyboard Control:
+     * - Handles the spacebar keydown event for toggling play/pause, ensuring the key press is valid only within the context of the component.
+     * WaveSurfer Instance:
+     * - The `wsPlayer` instance provides direct access to WaveSurfer.js methods, such as playback control and region management.
+     * - Regions are handled using the `RegionsPlugin` for highlighting and looping selected portions of audio.
+     * Shadow DOM:
+     * - The component uses Shadow DOM to encapsulate styles and provide a scoped structure for rendering.
+     */
     interface WsAudioPlayer {
         "audio"?: string;
         "audioTitle"?: string;
@@ -60,6 +192,43 @@ export { LocalJSX as JSX };
 declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
+            /**
+             * This class represents a custom audio player component built using WaveSurfer.js, offering
+             * functionalities such as play/pause, looping, and region-based audio playback control.
+             * Properties managed within the component allow customization of audio appearance,
+             * behavior, and additional event handling for a seamless user experience.
+             * The component relies on the WaveSurfer.js library to render audio waveforms and manage audio playback.
+             * A variety of features including looping regions, event dispatching, and keyboard interactions are supported.
+             * Props:
+             * - `audio`: URL string of the audio file to be played.
+             * - `color`: String for the waveform color.
+             * - `progress`: String for the progress bar color (default: "#666666").
+             * - `theme`: String indicating the UI theme of the audio player (default: "basic").
+             * - `audioTitle`: Title of the audio track.
+             * - `duration`: Duration of the audio track (mutable).
+             * - `height`: String specifying the height of the waveform.
+             * - `resolution`: Resolution value for waveform rendering (default: 100).
+             * States:
+             * - `isPlaying`: Boolean indicating the current playback state of the audio.
+             * - `curTime`: Current playback time of the audio in string format.
+             * - `isLooping`: Boolean indicating whether loop mode is active.
+             * - `audioRegions`: Array representing the defined audio regions for looping or selection.
+             * Methods:
+             * - `playpause()`: Toggles between play and pause states for the current audio.
+             * - `toggleLoop()`: Enables or disables a full-track looping feature with visual regions.
+             * - `enableFullTrackLoop()`: Configures and starts a full-track loop sequence using WaveSurfer.js regions.
+             * - `setLoop(enable)`: Globally enables or disables the loop functionality for the audio player.
+             * - `create()`: Initializes the WaveSurfer player instance with essential configurations and event listeners.
+             * Event Emission:
+             * - Emits the `wsAudioPlaying` event when playback starts, sharing audio source details and title.
+             * Keyboard Control:
+             * - Handles the spacebar keydown event for toggling play/pause, ensuring the key press is valid only within the context of the component.
+             * WaveSurfer Instance:
+             * - The `wsPlayer` instance provides direct access to WaveSurfer.js methods, such as playback control and region management.
+             * - Regions are handled using the `RegionsPlugin` for highlighting and looping selected portions of audio.
+             * Shadow DOM:
+             * - The component uses Shadow DOM to encapsulate styles and provide a scoped structure for rendering.
+             */
             "ws-audio-player": LocalJSX.WsAudioPlayer & JSXBase.HTMLAttributes<HTMLWsAudioPlayerElement>;
         }
     }
--- src/components/ws-audio-player/ws-audio-player.tsx
+++ src/components/ws-audio-player/ws-audio-player.tsx
@@ -2,14 +2,64 @@ import { Component, Prop, Method, Element, State, Watch, h } from '@stencil/core
 import WaveSurfer from 'wavesurfer.js';
 import RegionsPlugin from 'wavesurfer.js/dist/plugins/regions.js';
 
-type AudioRegion = {
+
+/**
+ * Represents a defined region in an audio file with a start point, end point, and looping functionality.
+ */
+interface AudioRegion {
   start: number;
   end: number;
   loop: boolean;
-  update: (params: Partial<{ start: number; end: number; loop: boolean }>) => void;
-  remove: () => void;
-};
+  update: Function;
+  remove: Function;
+}
 
+/**
+ * This class represents a custom audio player component built using WaveSurfer.js, offering
+ * functionalities such as play/pause, looping, and region-based audio playback control.
+ *
+ * Properties managed within the component allow customization of audio appearance,
+ * behavior, and additional event handling for a seamless user experience.
+ *
+ * The component relies on the WaveSurfer.js library to render audio waveforms and manage audio playback.
+ * A variety of features including looping regions, event dispatching, and keyboard interactions are supported.
+ *
+ * Props:
+ * - `audio`: URL string of the audio file to be played.
+ * - `color`: String for the waveform color.
+ * - `progress`: String for the progress bar color (default: "#666666").
+ * - `theme`: String indicating the UI theme of the audio player (default: "basic").
+ * - `audioTitle`: Title of the audio track.
+ * - `duration`: Duration of the audio track (mutable).
+ * - `height`: String specifying the height of the waveform.
+ * - `resolution`: Resolution value for waveform rendering (default: 100).
+ *
+ * States:
+ * - `isPlaying`: Boolean indicating the current playback state of the audio.
+ * - `curTime`: Current playback time of the audio in string format.
+ * - `isLooping`: Boolean indicating whether loop mode is active.
+ * - `audioRegions`: Array representing the defined audio regions for looping or selection.
+ *
+ * Methods:
+ * - `playpause()`: Toggles between play and pause states for the current audio.
+ * - `toggleLoop()`: Enables or disables a full-track looping feature with visual regions.
+ * - `enableFullTrackLoop()`: Configures and starts a full-track loop sequence using WaveSurfer.js regions.
+ * - `setLoop(enable)`: Globally enables or disables the loop functionality for the audio player.
+ * - `create()`: Initializes the WaveSurfer player instance with essential configurations and event listeners.
+ *
+ * Event Emission:
+ * - Emits the `wsAudioPlaying` event when playback starts, sharing audio source details and title.
+ *
+ * Keyboard Control:
+ * - Handles the spacebar keydown event for toggling play/pause, ensuring the key press is valid only within the context of the component.
+ *
+ * WaveSurfer Instance:
+ * - The `wsPlayer` instance provides direct access to WaveSurfer.js methods, such as playback control and region management.
+ * - Regions are handled using the `RegionsPlugin` for highlighting and looping selected portions of audio.
+ *
+ * Shadow DOM:
+ * - The component uses Shadow DOM to encapsulate styles and provide a scoped structure for rendering.
+ */
 @Component({
   tag: 'ws-audio-player',
   styleUrl: 'ws-audio-player.css',
@@ -39,6 +89,11 @@ export class WSAudioPlayer {
     this.wsPlayer.load(newValue);
   }
 
+  /**
+   * Toggles the playback state of the media player. If playback is currently ongoing, it will pause; otherwise, it will start/resume.
+   *
+   * @return {Promise<void>} A promise that resolves when the playback state toggling, state updates, and event emission are complete.
+   */
   @Method()
   async playpause(): Promise<void> {
     // Toggle playback
@@ -53,7 +108,11 @@ export class WSAudioPlayer {
   }
 
   /**
-   * Emit a global event to notify other components of playback
+   * Emits a custom event 'wsAudioPlaying' to notify listeners that an audio track is playing.
+   * The event includes details about the audio source, title, and the component emitting the event.
+   * The event bubbles up the DOM and crosses shadow DOM boundaries.
+   *
+   * @return {void} This method does not return a value.
    */
   private emitAudioPlayingEvent(): void {
     const event = new CustomEvent('wsAudioPlaying', {
@@ -67,7 +126,6 @@ export class WSAudioPlayer {
     });
     window.dispatchEvent(event);
   }
-
 
   /**
    * Handles the keydown event for spacebar toggle
@@ -89,6 +147,14 @@ export class WSAudioPlayer {
   }
 
 
+  /**
+   * Toggles the loop feature of the player. Enables or disables looping for the full track.
+   * When enabled, the loop creates a UI region and subscribes to the timeupdate event for
+   * continuous looping within the set region. When disabled, it clears the region and
+   * unsubscribes from the related event listeners.
+   *
+   * @return {Promise<void>} A promise that resolves when the looping state has been successfully toggled.
+   */
   @Method()
   async toggleLoop(): Promise<void> {
     // Retrieve the RegionsPlugin instance
@@ -121,6 +187,13 @@ export class WSAudioPlayer {
     }
   }
 
+  /**
+   * Enables a looping feature for the entire audio track using a region.
+   * The looping is manually managed through time updates.
+   * This method ensures a region is created to represent the loop and handles playback accordingly.
+   *
+   * @return {Promise<void>} A Promise that resolves when the loop has been successfully set up, or logs errors for invalid states.
+   */
   @Method()
   async enableFullTrackLoop(): Promise<void> {
     // Fetch the RegionsPlugin instance
@@ -169,11 +242,17 @@ export class WSAudioPlayer {
     };
     this.wsPlayer.on('timeupdate', this.timeUpdateListener); // Attach the listener
 
-
     // Explicitly start playback
     this.wsPlayer.play(loopStart);
   }
 
+  /**
+   * Enables or disables the loop feature for the media player.
+   * When enabled, the media will replay automatically after it finishes.
+   *
+   * @param {boolean} enable - A boolean flag to enable (true) or disable (false) the loop functionality.
+   * @return {Promise<void>} A promise that resolves when the loop functionality is successfully updated.
+   */
   @Method()
   async setLoop(enable: boolean): Promise<void> {
     this.wsPlayer.on('finish', () => {
@@ -185,6 +264,13 @@ export class WSAudioPlayer {
     }
   }
 
+  /**
+   * Creates a WaveSurfer player instance and initializes it with the specified options.
+   * Sets up event listeners for playback, audio processing, and global playback control.
+   * Initializes the audio waveform, duration, and playback state.
+   *
+   * @return {Promise<void>} A promise that resolves when the WaveSurfer player is ready.
+   */
   @Method()
   async create(): Promise<void> {
     const container = this.el.shadowRoot.querySelector('#wavesurfer') as HTMLElement; // Use a type assertion
@@ -241,21 +327,45 @@ export class WSAudioPlayer {
     });
   }
 
+  /**
+   * Calculates the width of a bar based on the current resolution.
+   * The bar width decreases linearly as the resolution increases,
+   * with a maximum width at a resolution of 0 and progressively smaller width up to a resolution of 100.
+   *
+   * @return {number} The calculated bar width, clamped within the allowed range.
+   */
   calculateBarWidth(): number {
     const maxBarWidth = 10; // Maximum bar width at resolution = 0
     const normalizedResolution = Math.min(Math.max(this.resolution, 0), 100); // Clamp resolution between 0 and 100
     return maxBarWidth * (1 - normalizedResolution / 100); // Linearly scale widthefault example value
   }
 
+  /**
+   * Lifecycle method that is invoked once the component has been loaded into the DOM.
+   * This method typically serves as a hook to perform initializations that require access to rendered elements.
+   *
+   * @return {void} Does not return a value.
+   */
   componentDidLoad() {
     this.create();
   }
 
+  /**
+   * Retrieves the current playback time from the media player.
+   *
+   * @return {string} The current playback time formatted as a string.
+   */
   getCurrentTime() {
     window.addEventListener('keydown', this.handleKeyDown);
     return this.formatTime(this.wsPlayer.getCurrentTime());
   }
 
+  /**
+   * Converts a given time in seconds to a formatted string in HH:MM:SS or MM:SS format.
+   *
+   * @param {number|string} time The time in seconds to be converted. It can be a numeric string or a number.
+   * @return {string} A formatted time string. If the hour part is 0, the format will be MM:SS; otherwise, HH:MM:SS.
+   */
   formatTime(time) {
     const sec_num = parseInt(time, 10);
     const hours = Math.floor(sec_num / 3600);
